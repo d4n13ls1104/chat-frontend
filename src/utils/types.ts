e9f424fbe8401b6a58a8b0fbfacc38fd822048c8
d4n13ls1104 <d4n13lsofficial@gmail.com>
2022-11-21 02:18:42 -0500

    add AuthResponse type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,9 +2,14 @@ export type CreateUserParams = {
     email: string;
     username: string;
     password: string;
-}
+};
 
 export type LoginUserParams = {
     email: string;
     password: string;
-}
+};
+
+export type AuthResponse = {
+    error?: string;
+    success?: string;
+};
