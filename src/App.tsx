b7f501e88ba91e02b38524e6a681596fdf53b62b
d4n13ls1104 <d4n13lsofficial@gmail.com>
2022-11-22 00:03:28 -0500

    add HomePage route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RegisterPage } from "./pages/RegisterPage";
 import { LoginPage } from "./pages/LoginPage";
 import "./index.css";
+import { HomePage } from "./pages/HomePage";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
             </Routes>
