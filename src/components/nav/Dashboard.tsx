4ef6498769a008069c84c2cbb93248dc64eaa312
d4n13ls1104 <d4n13lsofficial@gmail.com>
2022-11-22 00:03:58 -0500

    add user dashboard component

---
+++ src/components/nav/Dashboard.tsx
@@ -0,0 +1,30 @@
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import {
+    DashboardWrapper,
+    DashboardAvatar,
+    DashboardTextWrapper,
+    DashboardUsernameLabel,
+    DashboardHandleLabel,
+} from "../../utils/styles";
+
+export const Dashboard = () => {
+    return (
+        <DashboardWrapper>
+            <DashboardAvatar src="https://pbs.twimg.com/media/Eyjq7IBXMAAbxdX?format=jpg&name=medium" />
+            <DashboardTextWrapper>
+                <DashboardUsernameLabel>President Biden</DashboardUsernameLabel>
+
+                <DashboardHandleLabel>@POTUS</DashboardHandleLabel>
+                <BiDotsHorizontalRounded
+                    fill="#fff"
+                    size="20px"
+                    style={{
+                        position: "absolute",
+                        right: "16px",
+                        top: "24px",
+                    }}
+                />
+            </DashboardTextWrapper>
+        </DashboardWrapper>
+    );
+};
