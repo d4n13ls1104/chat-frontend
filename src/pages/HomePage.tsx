a91f6e58f57f8e57288c24ee5d3f06f634728635
d4n13ls1104 <d4n13lsofficial@gmail.com>
2022-11-22 00:04:37 -0500

    add HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from "../components/nav/Dashboard";
+import { NavSidebar } from "../components/nav/NavSidebar";
+
+export const HomePage = () => {
+    return (
+        <>
+            <NavSidebar />
+            <Dashboard />
+        </>
+    );
+};
