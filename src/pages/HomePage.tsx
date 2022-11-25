21ba6881c77f72f71cd68fcdc5e9bf01369573ae
d4n13ls1104 <d4n13lsofficial@gmail.com>
2022-11-25 00:54:10 -0500

    add RightPanel to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Dashboard } from "../components/nav/Dashboard";
 import { NavSidebar } from "../components/nav/NavSidebar";
+import { RightPanel } from "../components/nav/RightPanel";
 
 export const HomePage = () => {
     return (
         <>
             <NavSidebar />
             <Dashboard />
+            <RightPanel />
         </>
     );
 };
